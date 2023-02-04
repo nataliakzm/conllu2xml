7394b06c26c63f067e77ee9a36450fc5b941dba7
Natalia Kuzminykh <natkuzminykh94@gmail.com>
2023-02-04 23:33:25 +0100

    Structure is redone

--- setup.py
+++ setup.py
@@ -1,23 +1,34 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name='conllu2xml',
-    version='0.1',
-    description='Converts, clean and parse CoNLL-U files to XML and/or TRF files',
-    url='#',
-    author='Natalia Kuzminykh',
-    author_email='	contact@nataliakzm.codes',
-    license='MIT',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    install_requires=[
-        'conllu2xml',
-        'conlluparse',
-        'argparse',
-        'os-sys',
-        'regex',
-        'stanza',
-        'nltk'
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="conllu2xml",
+    version="1.0.0",
+
+    author="Natalia Kuzminykh",
+    author_email="	contact@nataliakzm.codes",
+
+    description="Converts, clean and parse CoNLL-U files to XML and/or TRF files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nataliakzm/conllu2xml",
+    
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
+    install_requires=[
+        "argparse",
+        "os-sys",
+        "regex",
+        "stanza",
+        "nltk"
+    ],
+
+    python_requires='>=3.8.10',
     zip_safe=False
 )
