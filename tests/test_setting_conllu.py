2099335702e44e4d3fc9a6eeaba7c13c525ea388
Natalia Kuzminykh <natkuzminykh94@gmail.com>
2023-04-04 11:57:59 +0200

    Tests updated

--- tests/test_setting_conllu.py
+++ tests/test_setting_conllu.py
@@ -1,61 +1,91 @@
 import os
-import pytest
+import tempfile
 from conllu2xml.conlluparse.setting_conllu import read_conllu, write_conllu, del_id, del_hashlines, set_id, change_divider
 
-@pytest.fixture
-def sequoia_lines():
-    file_path = os.path.join(os.path.dirname(__file__), 'sequoia-ud.conllu')
-    return read_conllu(file_path)
+def test_read_conllu_write_conllu():
+    # Create a temporary file and write the sample content
+    content = "Sample conllu content\n"
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as tmp_file:
+        tmp_file.write(content)
 
-def test_read_conllu(sequoia_lines):
-    assert len(sequoia_lines) > 0
+    # Check if the content is read correctly
+    lines = read_conllu(tmp_file.name)
+    assert lines == content.splitlines(True), f"Expected {content}, but got {lines}"
 
-def test_del_id(sequoia_lines):
-    lines = del_id(sequoia_lines)
-    for line in lines:
-        assert not line.startswith('# sent_id')
+    # Create another temporary file for writing the content using write_conllu()
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as tmp_file2:
+        write_conllu(tmp_file2.name, lines)
 
-def test_del_hashlines(sequoia_lines):
-    lines = del_hashlines(sequoia_lines)
-    for line in lines:
-        assert not line.startswith(tuple('# ' + s for s in ['newdoc', 'sent_id', 'text_en', 'genre', 'author', 'work',
-                               'global.Entity',  'orig_file_sentence', 'english_text', 'newpar',
-                                'sound_url', 'macrosyntax', 'status', 'title', 'speaker',
-                                'review-category', 'review-place', 'review-date', 
-                                'review-likes', 'review-dislikes']))
+        # Read the content back from the file
+        tmp_file2.seek(0)
+        written_content = tmp_file2.read()
 
-def test_set_id(sequoia_lines):
-    lines = set_id(sequoia_lines)
-    id_count = 0
-    for line in lines:
-        if line.startswith('# id ='):
-            id_count += 1
-            assert line.strip() == f'# id = {id_count}'
+    # Check if the content is written correctly
+    assert written_content == content, f"Expected {content}, but got {written_content}"
 
-def test_change_divider(sequoia_lines):
-    lines = change_divider(sequoia_lines)
-    for line in lines:
-        if not line.startswith('#'):
-            columns = line.split('\t')
-            if len(columns) >= 6:
-                assert '|' not in columns[5]
+    # Clean up temporary files
+    os.remove(tmp_file.name)
+    os.remove(tmp_file2.name)
 
-def test_write_conllu(sequoia_lines):
-    lines = del_id(sequoia_lines)
-    lines = del_hashlines(lines)
-    lines = set_id(lines)
-    lines = change_divider(lines)
+def test_del_id():
+    lines = [
+        "# sent_id = 1\n",
+        "This is a test.\n",
+        "# sent_id = 2\n",
+        "This is another test.\n"
+    ]
+    expected_output = [
+        "This is a test.\n",
+        "This is another test.\n"
+    ]
+    result = del_id(lines)
+    assert result == expected_output, f"Expected {expected_output}, but got {result}"
 
-    output_file_path = os.path.join(os.path.dirname(__file__), 'sequoia-ud_tested.conllu')
-    write_conllu(output_file_path, lines)
+def test_del_hashlines():
+    lines = [
+        "# newdoc\n",
+        "# sent_id = 1\n",
+        "This is a test.\n",
+        "# sent_id = 2\n",
+        "This is another test.\n"
+    ]
+    expected_output = [
+        "This is a test.\n",
+        "This is another test.\n"
+    ]
+    result = del_hashlines(lines)
+    assert result == expected_output, f"Expected {expected_output}, but got {result}"
 
-    # Check if the file is created
-    assert os.path.exists(output_file_path)
+def test_set_id():
+    lines = [
+        "# text = This is a test.\n",
+        "This is a test.\n",
+        "# text = This is another test.\n",
+        "This is another test.\n"
+    ]
+    expected_output = [
+        "# id = 1\n",
+        "# text = This is a test.\n",
+        "This is a test.\n",
+        "# id = 2\n",
+        "# text = This is another test.\n",
+        "This is another test.\n"
+    ]
+    result = set_id(lines)
+    assert result == expected_output, f"Expected {expected_output}, but got {result}"
 
-    # Read the created file and check its content
-    new_lines = read_conllu(output_file_path)
-    assert len(new_lines) > 0
-    assert new_lines == lines
-
-    # Clean up by removing the created file
-    #os.remove(output_file_path)
+def test_change_divider():
+    lines = [
+        "# id = 1\n",
+        "le\t_\tle\tDET\t_\tDefinite=Def|Gender=Masc|Number=Sing|PronType=Art\t7\tdet\t_\t_\n",
+        "# id = 2\n",
+        "XIIe\tXIIe\tADJ\t_\tNumType=Ord\t9\tamod\t_\t_\n"
+    ]
+    expected_output = [
+        "# id = 1\n",
+        "le\t_\tle\tDET\t_\tDefinite=Def:Gender=Masc:Number=Sing:PronType=Art\t7\tdet\t_\t_\n",
+        "# id = 2\n",
+        "XIIe\tXIIe\tADJ\t_\tNumType=Ord\t9\tamod\t_\t_\n"
+    ]
+    result = change_divider(lines)
+    assert result == expected_output, f"Expected {expected_output}, but got {result}"
